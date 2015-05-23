363da9245338f8f18dc13c40e351726e89138748
Diego Hernan Prinzi <diegoprinzi@gmail.com>
2015-05-23 17:55:27 -0300

    Added selection feature with random

--- WPFChallenge/MainWindow.xaml.cs
+++ WPFChallenge/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,53 @@ namespace WPFChallenge
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public string Options1 { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            OptionCircle1.Options = new List<string> { "one of a kind", "small batch", "large batch", "mass market" };
+            OptionCircle2.Options = new List<string> { "savory", "sweet", "umami" };
+            OptionCircle3.Options = new List<string> { "crunchy", "mushy", "smooth" };
+            OptionCircle4.Options = new List<string> { "spicy", "mild" };
+            OptionCircle5.Options = new List<string> { "a little", "a lot" };
+            OptionCircle6.Options = new List<string> { "breakfast", "brunch", "lunch", "snack", "dinner" };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void btnShuffle_Click(object sender, RoutedEventArgs e)
+        {
+            var optionCircles = GetLogicalChildCollection<OptionCircle>(mainGrid);
+            foreach (var optionCircle in optionCircles)
+            {
+                optionCircle.GetRandomValue();
+            }
+        }
+
+        private static List<T> GetLogicalChildCollection<T>(object parent) where T : DependencyObject
+        {
+            List<T> logicalCollection = new List<T>();
+            GetLogicalChildCollection(parent as DependencyObject, logicalCollection);
+            return logicalCollection;
+        }
+
+        private static void GetLogicalChildCollection<T>(DependencyObject parent, List<T> logicalCollection) where T : DependencyObject
+        {
+            IEnumerable children = LogicalTreeHelper.GetChildren(parent);
+            foreach (object child in children)
+            {
+                if (child is DependencyObject)
+                {
+                    DependencyObject depChild = child as DependencyObject;
+                    if (child is T)
+                    {
+                        logicalCollection.Add(child as T);
+                    }
+                    GetLogicalChildCollection(depChild, logicalCollection);
+                }
+            }
         }
     }
 }
---
+++ WPFChallenge/OptionCircle.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFChallenge
+{
+    /// <summary>
+    /// Interaction logic for OptionCircle.xaml
+    /// </summary>
+    public partial class OptionCircle : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Timer _timer;
+
+        private string _selectedOption;
+        public string SelectedOption
+        {
+            private set
+            {
+                _selectedOption = value;
+                OnPropertyChanged("SelectedOption");
+            }
+            get
+            {
+                return _selectedOption;
+            }
+        }
+
+        private List<string> _options;
+        public List<string> Options
+        {
+            set
+            {
+                _options = value.Select(x => x.ToUpper()).ToList();
+                SelectedOption = _options.First();
+            }
+            get
+            {
+                return _options;
+            }
+        }
+
+        public OptionCircle()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void btnCircle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _timer = new Timer(1000);
+            _timer.Elapsed += new ElapsedEventHandler(ChangeSelectedOption);
+            _timer.Enabled = true;
+            _timer.Start();
+        }
+
+        private void btnCircle_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _timer.Stop();
+            _timer.Dispose();
+        }
+
+        private void ChangeSelectedOption(object sender, ElapsedEventArgs e)
+        {
+            var currentIndex = Options.IndexOf(SelectedOption);
+            SelectedOption = currentIndex == Options.Count - 1 ? Options.First() : Options[currentIndex + 1];
+        }
+
+        public void GetRandomValue() { 
+            var rnd = new Random();
+            SelectedOption = Options[rnd.Next(Options.Count)];
+        }
+
+    }
+}
